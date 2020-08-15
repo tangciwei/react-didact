5ae4c6c6b6464553d55225d16854740d19335c7d
唐词伟 <tangciwei@bytedance.com>
2020-08-15 17:25:48 +0800

    feat: fiber
    Change-Id: I29c417f538476a5ec727642db98a51e143081d32

--- src/didact.ts
+++ src/didact.ts
@@ -17,8 +17,10 @@ interface VDom {
     type: ElementType;
     props: Props;
 }
+type DOM = Text | Element;
 
-// -------------
+
+// ----------------------------
 
 function createElement(type: ElementType, props: Props, ...children: Props['children']) {
     return {
@@ -41,24 +43,43 @@ function createTextElement(text: string) {
         },
     };
 }
-
-function render(element: VDom, container: Text | HTMLElement) {
-    const dom =
-        element.type == "TEXT_ELEMENT"
-            ? document.createTextNode("")
-            : document.createElement(element.type);
-    const isProperty = (key) => key !== "children";
-    Object.keys(element.props)
-        .filter(isProperty)
-        .forEach((name) => {
-            dom[name] = element.props[name];
-        });
-    element.props.children.forEach((child: VDom) => render(child, dom));
-    container.appendChild(dom);
+// ----------------------------
+interface Fiber {
+    props: Props;
+    type?: ElementType
+    dom?: DOM | null;
+    parent?: Fiber
+    child?: Fiber
+    sibling?: Fiber
 }
 
+function createDom(fiber): DOM {
+    const dom =
+        fiber.type == "TEXT_ELEMENT"
+            ? document.createTextNode("")
+            : document.createElement(fiber.type) as Element
 
-let nextUnitOfWork = null
+    const isProperty = key => key !== "children"
+    Object.keys(fiber.props)
+        .filter(isProperty)
+        .forEach(name => {
+            dom[name] = fiber.props[name]
+        })
+    return dom
+}
+
+let nextUnitOfWork: Fiber = null
+
+function render(element: VDom, container: DOM) {
+    nextUnitOfWork = {
+        dom: container,
+        props: {
+            children: [element],
+        },
+    }
+}
+
+// ----------------------------
 function workLoop(deadline: Deadline) {
     let shouldYield = false
     while (nextUnitOfWork && !shouldYield) {
@@ -72,11 +93,52 @@ function workLoop(deadline: Deadline) {
 
 requestIdleCallback(workLoop)
 
-function performUnitOfWork(nextUnitOfWork) {
-    // TODO
+
+function performUnitOfWork(fiber: Fiber): Fiber {
+    // 第一步，创建dom
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber)
+    }
+
+    if (fiber.parent) {
+        fiber.parent.dom.appendChild(fiber.dom)
+    }
+    // 第二步，children遍历
+    const elements = fiber.props.children as VDom[];
+    let index = 0
+    let prevSibling: Fiber = null
+
+    while (index < elements.length) {
+        const element = elements[index]
+
+        const newFiber: Fiber = {
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: null,
+        }
+        if (index === 0) {
+            fiber.child = newFiber
+        } else {
+            prevSibling.sibling = newFiber
+        }
+
+        prevSibling = newFiber
+        index++
+
+    }
+    // 第三步
+    if (fiber.child) {
+        return fiber.child
+    }
+    let nextFiber = fiber
+    while (nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling
+        }
+        nextFiber = nextFiber.parent
+    }
 }
-
-
 
 export const Didact = {
     createElement,
